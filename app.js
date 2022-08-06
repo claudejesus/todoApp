d1f0982d452cea58a175fd7cd66bc59c82e7dfd9
claudejesus <niyigabaclaujesus@gmail.com>
2022-08-06 21:16:42 +0200

    third commit

---
+++ app.js
@@ -0,0 +1,16 @@
+function sayHello(name) {
+    console.log('Hello ' + name);
+}
+
+sayHello('claude'); 
+//console.log(window);
+
+// console.log();
+// setTimeout();
+// clearTimeout();
+
+// setTimeout();
+// clearTimeout();
+
+var message = '';
+console.log(global.message);
